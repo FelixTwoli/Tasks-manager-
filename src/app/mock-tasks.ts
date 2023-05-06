e9ecd7298bde3937cd3d2b358c8f9962fc73b06a
Felix <felixtwoli21@gmail.com>
2023-05-06 13:03:34 +0300

    Re-organize folder structure

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from "./Task";
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Doctor appointment',
+    day: 'May 4th at 2pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Meeting at school',
+    day: 'May 5th at 1pm',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Food shopping',
+    day: 'May 6th at 12pm',
+    reminder: false,
+  },
+];
